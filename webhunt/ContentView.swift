b47eafdea5a7c15ddf396c4bc75a3ebaefba91b5
Shersheial Borisute <sharshib@gmail.com>
2020-04-28 09:25:01 -0400

    Initial Commit

---
+++ webhunt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  webhunt
+//
+//  Created by Sharshi Borisute on 4/28/20.
+//  Copyright © 2020 Sharshi.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
